245705b452d40792c88f6928c9e00e008796642b
Roman Dyakonov <i@rodrix.ru>
2024-09-08 04:05:56 +0900

    Add hero ranking

--- lib/features/home/presentation/widgets/hero_card_widget.dart
+++ lib/features/home/presentation/widgets/hero_card_widget.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dota2_heroes/features/home/presentation/widgets/hero_rank_widget.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../../../../core/constants/constants.dart';
 import '../../data/models/hero_model.dart';
@@ -12,10 +14,25 @@ class HeroCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10),
-        child: CachedNetworkImage(
-          imageUrl: "$baseImageURL${hero.img!}",
-          fit: BoxFit.fitWidth,
-        ));
+      padding: const EdgeInsets.all(5),
+      child: Column(
+        children: [
+          CachedNetworkImage(
+            imageUrl: "$baseImageURL${hero.img!}",
+            fit: BoxFit.fitWidth,
+          ),
+          HeroRankWidget(
+            win: hero.i8Win!,
+            pick: hero.i8Pick!,
+            assets: 'assets/rank_logo/rank8_0.png',
+          ),
+          HeroRankWidget(
+            win: hero.i1Win!,
+            pick: hero.i1Pick!,
+            assets: 'assets/rank_logo/rank1_1.png',
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/features/home/presentation/widgets/hero_rank_widget.dart
@@ -0,0 +1,65 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HeroRankWidget extends StatelessWidget {
+  final int win;
+  final int pick;
+  final String assets;
+
+  const HeroRankWidget(
+      {super.key, required this.win, required this.pick, required this.assets});
+
+  @override
+  Widget build(BuildContext context) {
+    dynamic resp = Calculator(WinRate(win, pick)).sum();
+    return Row(
+      children: [
+        SizedBox(
+          height: 25,
+          width: 25,
+          child: Image(image: AssetImage(assets)),
+        ),
+        Text("${resp[0] ? '↑' : '↓'}${resp[1].toString().substring(0, 5)}",
+            style: TextStyle(
+                color: resp[0] ? Colors.green : Colors.red,
+                fontWeight: FontWeight.bold)),
+        const Expanded(child: SizedBox()),
+      ],
+    );
+  }
+}
+
+class Shape extends Equatable {
+  final int win;
+  final int pick;
+
+  const Shape({required this.win, required this.pick});
+
+  area() {
+    throw UnimplementedError();
+  }
+
+  @override
+  List<Object> get props => [win, pick];
+}
+
+class WinRate extends Shape {
+  const WinRate(win, pick) : super(win: win, pick: pick);
+
+  @override
+  area() {
+    dynamic winRate = win / ((pick) / 100);
+    return [winRate < 50 ? false : true, winRate];
+  }
+}
+
+class Calculator {
+  final Shape shape;
+
+  Calculator(this.shape);
+
+  sum() {
+    return shape.area();
+  }
+}
