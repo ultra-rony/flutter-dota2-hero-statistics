7f9ec3db9c6f250a5bb2d2ad61cf358ec8184bf0
Roman Dyakonov <i@rodrix.ru>
2024-09-08 00:47:18 +0900

    Generate heroes_repository_impl.dart

---
+++ lib/features/home/data/repository/heroes_repository_impl.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+
+import 'package:dota2_heroes/core/resources/date_state.dart';
+import 'package:dota2_heroes/features/home/data/data_sources/remove/heroes_api_service.dart';
+import 'package:dota2_heroes/features/home/data/models/hero_model.dart';
+import 'package:dota2_heroes/features/home/domain/repository/heroes_repository.dart';
+
+class HeroesRepositoryImpl extends HeroesRepository {
+  final HeroesApiService _apiService;
+
+  HeroesRepositoryImpl(this._apiService);
+
+  @override
+  Future<DataState<List<HeroModel>>> getRemoteHeroes() async {
+    try {
+      final httpResponse = await _apiService.getHeroes();
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(DioError(
+                error: httpResponse.response.statusMessage,
+                response: httpResponse.response,
+                requestOptions: httpResponse.response.requestOptions)
+            .toString());
+      }
+    } on DioError catch (e) {
+      return DataFailed(e.toString());
+    }
+  }
+}
