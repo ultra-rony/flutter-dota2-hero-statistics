00934cc5ba1fc4820f45f97d384d7c361587732c
Roman Dyakonov <i@rodrix.ru>
2024-09-08 01:20:01 +0900

    Add bloc

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -3,6 +3,7 @@ import 'package:dota2_heroes/features/home/data/data_sources/remove/heroes_api_s
 import 'package:dota2_heroes/features/home/data/repository/heroes_repository_impl.dart';
 import 'package:dota2_heroes/features/home/domain/repository/heroes_repository.dart';
 import 'package:dota2_heroes/features/home/domain/usecases/get_remove_heroes_use_case.dart';
+import 'package:dota2_heroes/features/home/presentation/bloc/heroes_bloc.dart';
 import 'package:dota2_heroes/features/settings/cubit/theme_cubit.dart';
 import 'package:dota2_heroes/features/navigation/cubit/navigation_cubit.dart';
 import 'package:get_it/get_it.dart';
@@ -21,5 +22,5 @@ Future<void> initializeDependencies() async {
   //UseCases
   sl.registerSingleton<GetRemoveHeroesUseCase>(GetRemoveHeroesUseCase(sl()));
   // Blocs
-
+  sl.registerFactory<HeroesBloc>(() => HeroesBloc(sl()));
 }
