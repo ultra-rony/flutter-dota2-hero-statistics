44c61d5a994bb2c3045833e1c4b3362f6440809e
Roman Dyakonov <i@rodrix.ru>
2024-09-08 17:10:35 +0900

    Add get_local_hero_use_case.dart

---
+++ lib/features/home/domain/usecases/get_local_hero_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:dota2_heroes/features/home/domain/repository/heroes_repository.dart';
+
+import '../../../../core/usecases/use_case.dart';
+import '../../data/models/hero_model.dart';
+
+class GetLocalHeroUseCase implements UseCase<HeroModel, void> {
+  GetLocalHeroUseCase(this._heroesRepository);
+
+  final HeroesRepository _heroesRepository;
+
+  @override
+  Future<HeroModel?> call({void params}) {
+    return _heroesRepository.getLocalHero(params as int);
+  }
+}
