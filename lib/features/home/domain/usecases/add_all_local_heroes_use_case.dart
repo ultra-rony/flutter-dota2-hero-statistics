e346e7e4611ed249247fdec5941f3385da7af0c0
Roman Dyakonov <i@rodrix.ru>
2024-09-08 17:13:24 +0900

    Add add_all_local_heroes_use_case.dart

---
+++ lib/features/home/domain/usecases/add_all_local_heroes_use_case.dart
@@ -0,0 +1,15 @@
+
+import '../../../../core/usecases/use_case.dart';
+import '../../data/models/hero_model.dart';
+import '../repository/heroes_repository.dart';
+
+class AddAllLocalHeroesUseCase implements UseCase<void, void> {
+  AddAllLocalHeroesUseCase(this._heroesRepository);
+
+  final HeroesRepository _heroesRepository;
+
+  @override
+  Future<void> call({void params}) {
+    return _heroesRepository.addAllLocalHeroesBox(params as List<HeroModel>);
+  }
+}
