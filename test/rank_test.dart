a3eebb3dfe9c22acca97dc63fcdbda13a16bdba6
Roman Dyakonov <i@rodrix.ru>
2024-09-18 23:50:29 +0900

    Rank method

--- test/rank_test.dart
+++ test/rank_test.dart
@@ -1,10 +1,21 @@
 import 'package:dota2_heroes/features/home/presentation/widgets/hero_rank_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+List<List<dynamic>> rankData = [
+  [49, 100, [false, 49.0]],
+  [51, 100, [true, 51.0]],
+];
+
 void main() {
+  for (var answer in rankData) {
+    rank(answer[0], answer[1], answer[2]);
+  }
+}
+
+void rank(win, play, answer) {
   test("Ranking unit test", () {
-    final win = WinRate(49, 100);
-    final result = Calculator(win).sum();
-    expect(result, [false, 49.0]);
+    final rank = WinRate(win, play);
+    final result = Calculator(rank).sum();
+    expect(result, answer);
   });
 }
