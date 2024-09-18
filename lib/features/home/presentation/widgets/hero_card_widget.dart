f0873794c768b103317c20cb55ef8cff83749a53
Roman Dyakonov <i@rodrix.ru>
2024-09-19 06:22:09 +0900

    Remove import

--- lib/features/home/presentation/widgets/hero_card_widget.dart
+++ lib/features/home/presentation/widgets/hero_card_widget.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dota2_heroes/features/home/presentation/widgets/hero_rank_widget.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
--- lib/features/home/presentation/widgets/hero_details_content_widget.dart
+++ lib/features/home/presentation/widgets/hero_details_content_widget.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../../../core/constants/constants.dart';
 import '../../../../generated/l10n.dart';
