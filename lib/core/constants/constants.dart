07f44151998c736deebcf0ef03dc599af0119a68
Roman Dyakonov <i@rodrix.ru>
2024-09-19 19:09:23 +0900

    Add enum PrimaryAttributes

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,3 +1,5 @@
 const String baseURL = 'https://api.opendota.com';
 const String baseImageURL = 'https://cdn.cloudflare.steamstatic.com';
 const String hiveBoxHeroes = "box_heroes";
+
+enum PrimaryAttributes { str, agi, int, all }
