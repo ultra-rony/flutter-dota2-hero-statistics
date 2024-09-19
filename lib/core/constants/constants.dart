deba94ec726374671825a07b2261cef265661571
Roman Dyakonov <i@rodrix.ru>
2024-09-19 23:04:47 +0900

    Remove hero enums

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,5 +1,3 @@
 const String baseURL = 'https://api.opendota.com';
 const String baseImageURL = 'https://cdn.cloudflare.steamstatic.com';
 const String hiveBoxHeroes = "box_heroes";
-
-enum PrimaryAttributes { str, agi, int, all }
