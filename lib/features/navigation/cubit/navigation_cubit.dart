887826e73adb49ff6b17a4eb4e77ea3c22e6542f
Roman Dyakonov <i@rodrix.ru>
2024-09-21 23:37:23 +0900

    Rename variable and method

--- lib/features/navigation/cubit/navigation_cubit.dart
+++ lib/features/navigation/cubit/navigation_cubit.dart
@@ -3,7 +3,7 @@ import 'package:bloc/bloc.dart';
 class NavigationCubit extends Cubit<int> {
   NavigationCubit() : super(0);
 
-  void onSelectedIndexMenu(int index) {
-    emit(index);
+  void onNavBarSelectedMenu(int navBarSelectedIndex) {
+    emit(navBarSelectedIndex);
   }
 }
